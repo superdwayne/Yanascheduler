1d8ce18236166b9156e42dfe91ca19afc3142400
Dwayne Paisley-Marshall <dwayne.p-marshall@ffukn963-2.local>
2021-02-11 12:46:40 +0000

    cors

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 let Server;
 if (process.env.NODE_ENV === "production") {
-  Server = "https://yana-scheduler.herokuapp.com/api/people";
+  Server = "https://farfetch-cors.herokuapp.com/https://yana-scheduler.herokuapp.com/api/people";
 } else {
   Server = "https://farfetch-cors.herokuapp.com/https://yana-scheduler.herokuapp.com/api/people";
 }
