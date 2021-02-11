58b0a4feeef9bb6bb108e595c63e9d89e671beeb
Dwayne Paisley-Marshall <dwayne.p-marshall@ffukn963-2.local>
2021-02-11 14:55:03 +0000

    final change? exit

--- src/server.js
+++ src/server.js
@@ -8,6 +8,6 @@ const config = {
 if (config === "production") {
   Server = "https://yana-scheduler.herokuapp.com/api/people";
 } else {
-  Server = "http://localhost:8080/api/people";
+  Server = "https://yana-scheduler.herokuapp.com/api/people";
 }
 export default Server;  
