53b2af5b46375eddb6b1c7d617eed363c24c8255
Dwayne Paisley-Marshall <dwayne.p-marshall@ffukn963-2.local>
2021-02-12 08:23:32 +0000

    test

--- src/server.js
+++ src/server.js
@@ -3,6 +3,6 @@ let Server;
 if (process.env.NODE_ENV === "production") {
   Server = "https://yana-scheduler.herokuapp.com/";
 } else {
-  Server = "http://localhost:8000/";
+  Server = "https://yana-scheduler.herokuapp.com/";
 }
 export default Server;  
